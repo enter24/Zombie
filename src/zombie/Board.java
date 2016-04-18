742d87aea4023caa035122a8a757ae6644fdd2aa
Enter <Enter@46.239.135.45>
2016-04-18 11:42:33 +0200

    First

---
+++ src/zombie/Board.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zombie;
+
+/**
+ *
+ * @author Enter
+ */
+public class Board {
+    /*
+    ludzie
+    barykady lewo, prawo, góra
+    zombie lewo, prawo, góra
+    magazyn
+    szpital
+    fabryka
+    destylarnia
+    farma duza
+    farma mala
+    kuchnia
+    */
+    
+    
+}
